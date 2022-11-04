5d205f51ac8076dc740fe4f486afc40a18f60824
tkamoche <tkamoche@gmail.com>
2022-11-04 10:59:47 +0300

    linked with html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,39 @@
-function func1(){
-   $("#img1").fadeToggle(3000); 
-}
+$(document).ready(function(){ 
+   $("#design").click(function(){
+     $("#design").hide(1200);
+     $("#text").toggle(1200);
+     $("#text").show(1200);
+   })
+   $("#text").click(function(){
+     $("#text").hide(1200);
+     $("#design").toggle(1200);
+     $("#design").show(1200);
+   });
+ });
+
+ $(document).ready(function(){
+   $("#development_pic").click(function(){
+     $("#development_pic").hide(1200);
+     $("#dev-text").toggle(1200);
+     $("#dev-text").show(1200);
+   })
+   $("#dev-text").click(function(){
+     $("#dev-text").hide(1200);
+     $("#development_pic").toggle(1200);
+     $("#development_pic").show(1200);
+   });
+ });
+
+
+ $(document).ready(function(){
+   $("#product-management").click(function(){
+     $("#product-management").hide(1200);
+     $("#product-text").toggle(1200);
+     $("#product-text").show(1200);
+   })
+   $("#product-text").click(function(){
+     $("#product-text").hide(1200);
+     $("#product-management").toggle(1200);
+     $("#product-management").show(1200);
+   });
+ });
